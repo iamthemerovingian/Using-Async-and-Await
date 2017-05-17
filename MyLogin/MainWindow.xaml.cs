ab9f09e61b342f9a4fb84f38fda4e36b231ea4de
Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-05-17 11:04:56 +0700

    set to initial

--- MyLogin/MainWindow.xaml.cs
+++ MyLogin/MainWindow.xaml.cs
@@ -14,49 +14,9 @@ namespace MyLogin
             InitializeComponent();
         }
 
-        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                LoginButton.IsEnabled = false;
-                BusyIndicator.Visibility = Visibility.Visible;
-
-                var result = await LoginAsync();
-
-                LoginButton.Content = result;
-
-                LoginButton.IsEnabled = true;
-                BusyIndicator.Visibility = Visibility.Hidden;
-            }
-            catch (Exception)
-            {
-                LoginButton.Content = "Internal Error!";
-            }
-        }
-
-        private async Task<string> LoginAsync()
-        {
-            try
-            {
-                var loginTask = Task.Run(() => {
-
-                    Thread.Sleep(2000);
-
-                    return "Login Successful!";
-                });
-                
-                var logTask = Task.Delay(2000); // Log the login
-                
-                var purchaseTask = Task.Delay(1000); // Fetch purchases
-
-                await Task.WhenAll(loginTask, logTask, purchaseTask);
-                
-                return loginTask.Result;
-            }
-            catch (Exception)
-            {
-                return "Login failed!";
-            }
+            
         }
     }
 }
