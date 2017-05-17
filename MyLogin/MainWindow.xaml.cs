c2caadc5495ee554579360d7e8bdc89326ea19fc
Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-05-17 14:26:05 +0700

    Added many paralell tasks that will execute and a line to await till all those paralell tasks are done.
    Await Task.WhenAll() will wait till all the paralell tasks are finished.

--- MyLogin/MainWindow.xaml.cs
+++ MyLogin/MainWindow.xaml.cs
@@ -42,7 +42,7 @@ namespace MyLogin
             {
                 //throw new UnauthorizedAccessException();
 
-                var result = await Task.Run(() =>
+                var loginTask = Task.Run(() =>
                 {
                     //throw new UnauthorizedAccessException();
 
@@ -50,7 +50,13 @@ namespace MyLogin
                     return "Login Successful!!";
                 });
 
-                return result;
+                var logTask = Task.Delay(2000); //Log the login with the server.
+
+                var purchaseTask = Task.Delay(1000); //Load the customer's files.
+
+                await Task.WhenAll(loginTask, logTask, purchaseTask);
+
+                return loginTask.Result;
             }
             catch (Exception e)
             {
